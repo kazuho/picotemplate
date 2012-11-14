c89c5289ba1a575bbc662e1c22c2051bcb63d032
Kazuho Oku <kazuhooku@gmail.com>
2012-11-14 15:58:01 +0900

    introduce <?! ?> (void expression)

--- picotemplate.pl
+++ picotemplate.pl
@@ -7,7 +7,7 @@ my $srcfn = shift @ARGV;
 die "no filename"
     unless $srcfn;
 
-my ($push_expr, $push_str) = convfunc($srcfn);
+my ($push_expr, $push_void_expr, $push_str) = convfunc($srcfn);
 
 $srcfn =~ m{^(.*/)_([^/]+)$}
     or die "filename should start with an underscore";
@@ -22,9 +22,13 @@ while (my $line = <$srcfh>) {
     if ($line =~ /^\?/) {
         my $markup = $';
         my @st;
-        for my $token (split /(<\?=.*?\?>)/, $markup) {
-            if ($token =~ /^\<\?=\s*(.*?)\s*\?>$/) {
-                push @st, $push_expr->($1);
+        for my $token (split /(<\?[=!].*?\?>)/, $markup) {
+            if ($token =~ /^\<\?([=!])\s*(.*?)\s*\?>$/) {
+                if ($1 eq '=') {
+                    push @st, $push_expr->($2);
+                } else {
+                    push @st, $push_void_expr->($2);
+                }
             } elsif ($token ne "") {
                 push @st, $push_str->($token);
             }
@@ -48,6 +52,11 @@ sub convfunc {
                 my $src = shift;
                 return qq{\$_ = ($src); s/\\n\$//; \$output .= \$_;};
             },
+            # push_void_expr function
+            sub {
+                my $src = shift;
+                return qq{do { $src };};
+            },
             # push_str function
             sub {
                 my $str = shift;
@@ -61,14 +70,19 @@ sub convfunc {
             # push_expr function
             sub {
                 my $src = shift;
-                return qq^{std::string _r($src); if (! _r.empty() && _r.back() == '\\n') _r.erase(_r.size() - 1); _.append(_r);}^;
+                return qq^{std::string _r; _r += ($src); if (! _r.empty() && *(_r.end() - 1) == '\\n') _r.erase(_r.size() - 1); _ += _r;}^;
+            },
+            # push_void_expr function
+            sub {
+                my $src = shift;
+                return qq{{std::string::size_type __picotemplate_l = _.size(); { $src; } if (__picotemplate_l != _.size() && *(_.end() - 1) == '\\n') _.erase(_.size() - 1);}};
             },
             # push_str function
             sub {
                 my $str = shift;
                 $str =~ s/([\\'"])/\\$1/gs;
                 $str =~ s/\n/\\n/gs;
-                return qq{_.append("$str");};
+                return qq{_ += ("$str");};
             },
         );
     } elsif ($srcfn =~ /\.jsx?$/) {
@@ -78,6 +92,11 @@ sub convfunc {
             sub {
                 my $src = shift;
                 return qq{_ += ($src).replace(/\\n\$/, "");};
+            },
+            # push_void_expr function
+            sub {
+                my $src = shift;
+                return qq{{ $src; }};
             },
             # push_str function
             sub {
